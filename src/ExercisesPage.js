42757653d62d7c6cc03dabb71e6cf84d0c16b1a8
Kurt Vermillion <kurtv0727@gmail.com>
2022-11-02 15:51:04 -0400

    init

---
+++ src/ExercisesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewExerciseForm from './NewExerciseForm'
+
+export default function ExercisesPage() {
+  return (
+    <main>
+      <NewExerciseForm/>
+    </main>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <img class="header" src="./images/brosplitspro.png" alt="Logo" />
+                <img class="logo" src="./images/logo.jpg" alt="Logo" />
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
