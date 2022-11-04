eca9a815c7cf7f81349890c8b9a1bfa6582259bb
Kurt Vermillion <kurtv0727@gmail.com>
2022-11-04 12:10:48 -0400

    favorites stuff

--- src/ExerciseCard.js
+++ src/ExerciseCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import image1 from "./darkarm.png"
+import image2 from "./lightarm.jpg"
 
 function ExerciseCard({ exercise, onDeleteExercise }) {
 
-    const { image, name, bodypart, id } = exercise
-
+    const { image, name, bodypart, id, isFavorited } = exercise
+    
     function onClickDelete() {
         fetch(`http://localhost:3000/exercises/${id}`, {
             method: "DELETE",
@@ -17,9 +19,14 @@ function ExerciseCard({ exercise, onDeleteExercise }) {
             <h4>{name}</h4>
             <p>Body Part: {bodypart}</p>
             <button className='cardbtn'>Video</button>
-            
             <button className='cardbtn' onClick={onClickDelete}>Delete</button>
-
+            <div className="favorites">
+                {isFavorited ? (
+                    <button className='emoji-button-favorite-active'><img src={image2}/></button>
+                ) : (
+                    <button className='emoji-button-favorite'><img src={image1}/></button>
+                )}
+            </div>
         </li>
     );
 }
